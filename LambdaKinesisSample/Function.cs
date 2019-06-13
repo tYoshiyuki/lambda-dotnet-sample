ef47a451f1049183ca52cbd4fa5733cca82d18ca
ytsunomoto <ytsunomoto@tech.softbank.co.jp>
2019-06-13 23:50:18 +0900

    update

--- LambdaKinesisSample/Function.cs
+++ LambdaKinesisSample/Function.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections;
 using System.IO;
+using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
+using Amazon.Kinesis;
+using Amazon.Kinesis.Model;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.KinesisEvents;
 
@@ -12,22 +16,64 @@ namespace LambdaKinesisSample
 {
     public class Function
     {
+        private readonly string _streamName = "sample-kinesis-stream";
+        private readonly string _serviceURL = "http://localhost:4567/";
 
-        public void FunctionHandler(KinesisEvent kinesisEvent, ILambdaContext context)
+        public async Task FunctionHandler(KinesisEvent kinesisEvent, ILambdaContext context)
         {
             context.Logger.LogLine($"Beginning to process {kinesisEvent.Records.Count} records...");
 
+            // Kinesis Eventからレコードを取得し、ループ処理を行う
             foreach (var record in kinesisEvent.Records)
             {
                 context.Logger.LogLine($"Event ID: {record.EventId}");
                 context.Logger.LogLine($"Event Name: {record.EventName}");
 
-                string recordData = GetRecordContents(record.Kinesis);
+                var recordData = GetRecordContents(record.Kinesis);
                 context.Logger.LogLine($"Record Data:");
                 context.Logger.LogLine(recordData);
             }
 
-            context.Logger.LogLine("Stream processing complete.");
+            // Kinesis Stream の作成
+            var request = new CreateStreamRequest {
+                ShardCount = 1,
+                StreamName = _streamName
+            };
+
+            var client = new AmazonKinesisClient(new AmazonKinesisConfig {
+                ServiceURL = _serviceURL
+            });
+
+            var response = await client.ListStreamsAsync();
+            if (!response.StreamNames.Any(_ => _ == _streamName))
+            {
+                await client.CreateStreamAsync(request);
+            }
+
+            // Kinesis に対してデータを書き込む
+            foreach(var i in Enumerable.Range(1, 10))
+            {
+                using (var memory = new MemoryStream(Encoding.UTF8.GetBytes($"Put Data:{i}")))
+                {
+                    try
+                    {
+                        var req = new PutRecordRequest
+                        {
+                            StreamName = _streamName,
+                            PartitionKey = "url-response-times",
+                            Data = memory
+                        };
+
+                        var res = await client.PutRecordAsync(req);
+                        context.Logger.LogLine($"Successfully sent record to Kinesis. Sequence number: {res.SequenceNumber}.");
+                    }
+                    catch (Exception ex)
+                    {
+                        context.Logger.LogLine($"Failed to send record to Kinesis. Exception: {ex.Message}.");
+                    }
+                }
+                context.Logger.LogLine("Stream processing complete.");
+            }
         }
 
         private string GetRecordContents(KinesisEvent.Record streamRecord)
