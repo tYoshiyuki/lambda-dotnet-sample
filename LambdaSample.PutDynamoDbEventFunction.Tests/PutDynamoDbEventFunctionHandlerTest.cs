bfb3a820cd9b28bceedde20d12d9d7bb12899aa7
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2021-04-20 09:22:39 +0900

    update 更新。

---
+++ LambdaSample.PutDynamoDbEventFunction.Tests/PutDynamoDbEventFunctionHandlerTest.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.Lambda.TestUtilities;
+using LambdaSample.PutDynamoDbEventFunction.Models;
+using LambdaSample.PutDynamoDbEventFunction.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace LambdaSample.PutDynamoDbEventFunction.Tests
+{
+    public class PutDynamoDbEventFunctionHandlerTest
+    {
+        [Test]
+        public void Handle_正常系()
+        {
+            // Arrange
+            var mock = new Mock<ISampleUserService>();
+            mock.Setup(x => x.Create(It.IsAny<SampleUser>()));
+            var handler = new PutDynamoDbEventFunctionHandler(mock.Object);
+            var context = new TestLambdaContext();
+
+            // Act
+            handler.Handle(new PutDynamoDbEventFunctionInput { Key1 = "hello world" }, context);
+
+            // Assert
+            mock.Verify(x => x.Create(It.IsAny<SampleUser>()), Times.Once());
+        }
+    }
+}
